e13bc9ed5e10820fc0ebc538b5bb0f82563f728c
João Kax <lukaxsilva.ls@gmail.com>
2023-04-09 12:40:46 -0300

    fix: comments removed

--- src/main/java/com/example/appfinanceiro/AppFinanceiroApplication.java
+++ src/main/java/com/example/appfinanceiro/AppFinanceiroApplication.java
@@ -14,9 +14,4 @@ public class AppFinanceiroApplication {
         System.out.println("Documentation :  http://localhost:8080/swagger-ui/index.html#");
     }
 
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-
 }
